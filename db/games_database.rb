6c0c1edd941037809543fa586f9e2bd7f45ee146
vitor-vfm <vitor.vfm@gmail.com>
2017-04-02 19:28:24 -0600

    More pre- and post-conditions in the DB

--- db/games_database.rb
+++ db/games_database.rb
@@ -1,7 +1,7 @@
-require 'file'
-
 require 'test/unit/assertions'
 include Test::Unit::Assertions
+
+require 'json'
 
 class GamesDatabase
 
@@ -64,21 +64,69 @@ class GamesDatabase
         check_class_invariants
     end
 
+    def create_pre_cond(new_object)
+        assert(new_object.respond_to?(:to_json), 'New object must be convertible to json')
+        assert(new_object.key?(:type), 'New object must have a type')
+        assert(OBJECT_TYPES.include?(new_object[:type]), "#{new_object[:type]} is not one of the valid types")
+    end
+
+    def create_post_cond(new_object, id)
+        assert_not_empty(query(new_object.merge({:id => id})), 'New object was not stored')
+    end
+
     def create(new_object)
+        create_pre_cond(new_object)
         # TODO
+        id = 0
+        create_post_cond(new_object, id)
+        check_class_invariants
+    end
+
+    def update_pre_cond(id, object_content)
+        assert_not_empty(query({:id => id}), "There must be an object with id #{id}")
+    end
+
+    def update_post_cond(id, object_content)
+        objs = query(object_content.merge({:id => id}))
+        assert_not_empty(objs, 'Object should have been inserted in DB')
+        assert(objs.length == 1, 'Object must be unique in the DB')
     end
 
     def update(id, object_content)
+        update_pre_cond(id, object_content)
         # TODO
-        # Also does creation
+        update_post_cond(id, object_content)
+        check_class_invariants
+    end
+
+    def delete_pre_cond(id)
+        assert_not_empty(query({:id => id}), "There must be an object with id #{id}")
+    end
+
+    def delete_post_cond(id)
+        assert_empty(query({:id => id}), "Object with id #{id} should have been removed")
     end
 
     def delete(id)
+        delete_pre_cond(id)
         # TODO
+        delete_post_cond(id)
+        check_class_invariants
+    end
+
+    def delete_if_pre_cond(query_hash)
+        valid_query_hash(query_hash)
+    end
+
+    def delete_if_post_cond(query_hash)
+        assert_empty(query(query_hash), 'Object should have been removed')
     end
 
     def delete_if(query_hash)
+        delete_if_pre_cond(query_hash)
         # TODO
+        delete_if_post_cond(query_hash)
+        check_class_invariants
     end
 
     def query(query_hash)
@@ -92,5 +140,10 @@ class GamesDatabase
     def top_players(n)
         # TODO
         # pre-made query that returns the top n players and their scores
+    end
+
+    private
+    def valid_query_hash(query_hash)
+        assert(query_hash.respond_to?(:[]), 'Query must respond to square bracket operator')
     end
 end
