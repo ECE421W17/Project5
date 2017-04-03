6bce0b12979c48a7e45b827971c37c4c40abba16
vitor-vfm <vitor.vfm@gmail.com>
2017-04-02 20:13:18 -0600

    Move pre and post conditions to bottom of class, in a private section

--- db/games_database.rb
+++ db/games_database.rb
@@ -10,6 +10,85 @@ class GamesDatabase
     STOPPED_GAME_FILE = 'stopped_game.json'
     GAME_RESULT_FILE = 'game_result.json'
 
+    def initialize
+        initialize_pre_cond
+        @stopped_cache = {}
+        @result_cache = {}
+        # TODO
+        load_from_files
+        initialize_post_cond
+        check_class_invariants
+    end
+
+    def load_from_files
+        load_from_files_pre_cond
+        # TODO
+        # Creates the files if they don't exist
+        load_from_files_post_cond
+        check_class_invariants
+    end
+
+    def store_to_files
+        store_to_files_pre_cond
+        # TODO
+        store_to_files_post_cond
+        check_class_invariants
+    end
+
+    def create(new_object)
+        create_pre_cond(new_object)
+        # TODO
+        id = 0
+        create_post_cond(new_object, id)
+        check_class_invariants
+    end
+
+    def update(id, object_content)
+        update_pre_cond(id, object_content)
+        # TODO
+        update_post_cond(id, object_content)
+        check_class_invariants
+    end
+
+    def delete(id)
+        delete_pre_cond(id)
+        # TODO
+        delete_post_cond(id)
+        check_class_invariants
+    end
+
+    def delete_if(query_hash)
+        delete_if_pre_cond(query_hash)
+        # TODO
+        delete_if_post_cond(query_hash)
+        check_class_invariants
+    end
+
+    def query(query_hash)
+        query_pre_cond(query_hash)
+        # TODO
+        ret = [query_hash]
+        query_post_cond(query_hash, ret)
+        check_class_invariants
+    end
+
+    def aggregate_query(query_hash, field, &aggregating_function)
+        aggregate_query_pre_cond(query_hash, field, aggregating_function)
+        # TODO
+        ret = query_hash.merge({:result => 0})
+        aggregate_query_post_cond(query_hash, field, aggregating_function, ret)
+    end
+
+    def top_players(n)
+        top_players_pre_cond(n)
+        # pre-made query that returns the top n players and their scores
+        # TODO
+        ret = []
+        top_players_post_cond(n, ret)
+    end
+
+    private
+
     def check_class_invariants
         assert(@stopped_cache, 'There must be a stopped games cache')
         assert(@result_cache, 'There must be a games results cache')
@@ -23,28 +102,10 @@ class GamesDatabase
     def initialize_post_cond
     end
 
-    def initialize
-        initialize_pre_cond
-        @stopped_cache = {}
-        @result_cache = {}
-        # TODO
-        load_from_files
-        initialize_post_cond
-        check_class_invariants
-    end
-
     def load_from_files_pre_cond
     end
 
     def load_from_files_post_cond
-    end
-
-    def load_from_files
-        load_from_files_pre_cond
-        # TODO
-        # Creates the files if they don't exist
-        load_from_files_post_cond
-        check_class_invariants
     end
 
     def store_to_files_pre_cond
@@ -57,13 +118,6 @@ class GamesDatabase
     def store_to_files_post_cond
     end
 
-    def store_to_files
-        store_to_files_pre_cond
-        # TODO
-        store_to_files_post_cond
-        check_class_invariants
-    end
-
     def create_pre_cond(new_object)
         assert(new_object.respond_to?(:to_json), 'New object must be convertible to json')
         assert(new_object.key?(:type), 'New object must have a type')
@@ -72,14 +126,6 @@ class GamesDatabase
 
     def create_post_cond(new_object, id)
         assert_not_empty(query(new_object.merge({:id => id})), 'New object was not stored')
-    end
-
-    def create(new_object)
-        create_pre_cond(new_object)
-        # TODO
-        id = 0
-        create_post_cond(new_object, id)
-        check_class_invariants
     end
 
     def update_pre_cond(id, object_content)
@@ -92,26 +138,12 @@ class GamesDatabase
         assert(objs.length == 1, 'Object must be unique in the DB')
     end
 
-    def update(id, object_content)
-        update_pre_cond(id, object_content)
-        # TODO
-        update_post_cond(id, object_content)
-        check_class_invariants
-    end
-
     def delete_pre_cond(id)
         assert_not_empty(query({:id => id}), "There must be an object with id #{id}")
     end
 
     def delete_post_cond(id)
         assert_empty(query({:id => id}), "Object with id #{id} should have been removed")
-    end
-
-    def delete(id)
-        delete_pre_cond(id)
-        # TODO
-        delete_post_cond(id)
-        check_class_invariants
     end
 
     def delete_if_pre_cond(query_hash)
@@ -122,13 +154,6 @@ class GamesDatabase
         assert_empty(query(query_hash), 'Object should have been removed')
     end
 
-    def delete_if(query_hash)
-        delete_if_pre_cond(query_hash)
-        # TODO
-        delete_if_post_cond(query_hash)
-        check_class_invariants
-    end
-
     def query_pre_cond(query_hash)
         valid_query_hash(query_hash)
     end
@@ -136,16 +161,13 @@ class GamesDatabase
     def query_post_cond(query_hash, ret)
         ret.each do |obj|
             assert(query_hash.each_pair.all? {|k,v| obj.key?(k) && obj[k] == v},
-            "Object in return value #{obj} doesn't match query")
+                   "Object in return value #{obj} doesn't match query")
         end
     end
 
-    def query(query_hash)
-        query_pre_cond(query_hash)
-        # TODO
-        ret = [query_hash]
-        query_post_cond(query_hash, ret)
-        check_class_invariants
+    def valid_query_hash(query_hash)
+        assert(query_hash.respond_to?(:[]), 'Query must respond to square bracket operator')
+        assert(query_hash.respond_to?(:each_pair), 'Query must be enumerable by pairs')
     end
 
     def aggregate_query_pre_cond(query_hash, field, func)
@@ -157,13 +179,6 @@ class GamesDatabase
         assert(query_hash.each_pair.all? {|k,v| ret.key?(k) && ret[k] == v},
                "Object in return value #{ret} doesn't match query")
         assert(ret.key?(:result), 'The return value should have a :result field')
-    end
-
-    def aggregate_query(query_hash, field, &aggregating_function)
-        aggregate_query_pre_cond(query_hash, field, aggregating_function)
-        # TODO
-        ret = query_hash.merge({:result => 0})
-        aggregate_query_post_cond(query_hash, field, aggregating_function, ret)
     end
 
     def top_players_pre_cond(n)
@@ -185,20 +200,5 @@ class GamesDatabase
                 assert(ret[i][:score] >= ret[i-1][:score], 'Scores in return value are not monotonic')
             end
         end
-    end
-
-    def top_players(n)
-        top_players_pre_cond(n)
-        # pre-made query that returns the top n players and their scores
-        # TODO
-        ret = []
-        top_players_post_cond(n, ret)
-    end
-
-    private
-
-    def valid_query_hash(query_hash)
-        assert(query_hash.respond_to?(:[]), 'Query must respond to square bracket operator')
-        assert(query_hash.respond_to?(:each_pair), 'Query must be enumerable by pairs')
     end
 end
