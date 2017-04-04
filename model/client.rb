7dd8fae5e436bef548df5297e7fdb0b1db8e86bc
FreeStraint <rwu4@ualberta.ca>
2017-04-04 17:11:50 -0600

    push client

---
+++ model/client.rb
@@ -0,0 +1,29 @@
+
+class Client
+
+	def initialize(portNumber)
+
+	end
+
+	def startGame(mode)
+	end
+
+	def make_move(col)
+	end
+
+	def saveGame()
+
+	end
+
+	def loadGame(gameID)
+
+	end
+
+	def exitGame()
+
+	end
+
+
+
+
+end
