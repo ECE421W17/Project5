383d429ef5e19681cdf2f838d2d4bed27a04ef54
Andrew <adlawren@e5-05-10.ece.ualberta.ca>
2017-04-12 20:07:59 -0600

    Lab computer updates

--- db/launch_games_database_server.rb
+++ db/launch_games_database_server.rb
@@ -1,4 +1,10 @@
 require_relative 'games_database_server'
 
-gdbs = GamesDatabaseServer.new
+unless ARGV.length == 1
+  raise 'Missing argument; the port must be specified'
+end
+
+tmp1 = ARGV[0].to_i
+
+gdbs = GamesDatabaseServer.new(tmp1)
 gdbs.serve
